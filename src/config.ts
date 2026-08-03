a55d2867d15c1c754d0e8f9ca06be1a071dba39f
0x5t4ckc47 <noemail@email.com>
2026-08-03 18:38:07 +0800

    edit about and some config

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "0x5t4ckc47 Sec",
-	subtitle: "Security Notes & Writeups",
+	subtitle: "hacking Notes & Writeups",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -53,9 +53,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "0x5t4ckc47",
-	bio: "Security research, CTF notes, and reverse engineering writeups.",
+	bio: "RedTeam and Pentest learner.",
 	links: [
 		{
 			name: "GitHub",
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,13 +1,18 @@
 # About
+## 0x5t4ckc47
+你好, 这里是 *0x5t4ckc47*, 是一名等效996(796)的可怜的高中生. 
+你可以叫我 *白*, *栈猫*, 或者直接叫 *0x5t4ckc47*. 
 
-这里是 **0x5t4ckc47 Sec** 的个人博客。
+什么时候学习 Hacking 已经不记得了, 经历了两年多 GapYear, 正在复健.
+做过一些 CTF 题, 但不是一名 CTF 选手, 如果打 CTF 方向会是 Pwn, 不过目前还未纳入计划
 
-主要记录：
+一些账号:
+  1. HackTheBox: [0x5t4ckc47's HackTheBox Profile](https://profile.hackthebox.com/profile/019d62e2-878a-73c3-9497-f8d7c1df24e0)
+  2. TryHackMe: [0x5t4ckc47's TryHackMe Profile](https://tryhackme.com/p/0x5t4ckc47)
 
-- 安全研究与漏洞分析
-- CTF / Reverse Engineering writeups
-- 工具与学习笔记
+你可以在这里找到我(注: 这不是我的群): `25704224`
+
+*Happy Hacking*
 
 ::github{repo="0x5t4ckc47/0x5t4ckc47.github.io"}
 
-欢迎通过 GitHub 联系我。
